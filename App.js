8156ee8f631b711e82ff8d6b5c7aff36a2e09b60
Anders <andersmyrmel@gmail.com>
2023-02-08 18:57:36 +0100

    remove styles from app.js

--- App.js
+++ App.js
@@ -17,12 +17,3 @@ export default function App() {
 		</NavigationContainer>
 	);
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-});
