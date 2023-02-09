0916dfb3f93eb9ed7296c3dc9884d3b86bdddbe4
Shaheen Thayalan <shaheen@Shaheens-MBP.uia.no>
2023-02-09 18:19:24 +0100

    Updated the parts which required some kind of cleaness in the code

--- src/screens/SignUppage.js
+++ src/screens/SignUppage.js
@@ -1,86 +1,68 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import {
-	Text,
-	View,
-	TouchableOpacity,
-	StatusBar,
-	ScrollView,
-	ImageBackground,
-    TextInput,
-    Button,
-} from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { useNavigation } from '@react-navigation/core';
-import { styles } from '../styles/SignUpStyle';
-import LoginPage from './Loginpage';
+  Text,
+  View,
+  TouchableOpacity,
+  StatusBar,
+  ScrollView,
+  ImageBackground,
+  TextInput,
+  Button,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useNavigation } from "@react-navigation/core";
+import { styles } from "../styles/SignUpStyle";
+import LoginPage from "./LoginPage";
 
+const SignUpPage = ({ navigation }) => {
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
 
-const SignUpPage = () =>
-{
-    const navigation = useNavigation();
-
-    const goToHome = () => 
-    {
-		navigation.navigate('homepage');
-    }
-
-   
-
-    const [Email, setEmail] = useState("");
-    const [Password, setPassword] = useState("");
-
-    return (
-
+  return (
     <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Sign Up</Text>
 
-        <Text style={styles.title}>Sign Up</Text>
+      <View style={styles.inputViewEmail}>
+        <Text>email</Text>
+      </View>
+      <TextInput
+        style={styles.TextInputEmail}
+        onChangeText={(email) => setemail(email)}
+      />
 
-        
+      <View style={styles.inputViewPassword}>
+        <Text>password</Text>
+      </View>
+      <TextInput
+        style={styles.TextInputPassword}
+        onChangeText={(password) => setpassword(password)}
+      />
 
-        <View style={styles.inputViewEmail}>
-            <Text>Email</Text>    
-        </View>
-        <TextInput
-            style={styles.TextInputEmail}
-            onChangeText={(Email) => setEmail(Email)} />
+      <View style={styles.signupBtn}>
+        <Button
+          title="Sign Up"
+          color="white"
+          fontWeight="bold"
+          style={styles.signupBtn}
+          onPress={() => navigation.navigate("homepage")}
+        />
+      </View>
 
-        <View style={styles.inputViewPassword}>
-            <Text>Password</Text>
-        </View>
-        <TextInput
-            style={styles.TextInputPassword}
-            onChangeText={(Password) => setPassword(Password)}/>
+      <View style={styles.orText}>
+        <Text style={styles.orText}>⎯⎯⎯⎯⎯⎯⎯⎯OR⎯⎯⎯⎯⎯⎯⎯⎯</Text>
+      </View>
 
-        <View style={styles.signupBtn}>
-            <Button
-                title='Sign Up'
-                color="white"
-                fontWeight = "bold"
-                style={styles.signupBtn}
-                onPress={() => navigation.navigate("homepage")}/>
-        </View>
-        
-        <View style={styles.orText}>
-            <Text style={styles.orText}>⎯⎯⎯⎯⎯⎯⎯⎯OR⎯⎯⎯⎯⎯⎯⎯⎯</Text>
-        </View>
-
-        <View style={styles.loginBtn}>
-            <Button
-                title='LOGIN'
-                color="#2e216f"
-                fontWeight = "underline"
-                style={styles.loginBtn}
-                onPress={() => navigation.navigate("loginpage")}/>
-        </View>
-
-        
-
+      <View style={styles.loginBtn}>
+        <Button
+          title="LOGIN"
+          color="#2e216f"
+          fontWeight="underline"
+          style={styles.loginBtn}
+          onPress={() => navigation.navigate("loginpage")}
+        />
+      </View>
     </SafeAreaView>
-
-    );
-    
-
-
+  );
 };
 
 export default SignUpPage;
--- src/styles/SignUpStyle.js
+++ src/styles/SignUpStyle.js
@@ -1,100 +1,91 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-	},
+  container: {
+    flex: 1,
+  },
 
-    title: {
-        alignself: "center", 
-        position: "absolute",
-        right:155,
-        bottom: 539,
-        fontSize: 20,
-        fontWeight: "bold",
-    },
+  title: {
+    alignself: "center",
+    position: "absolute",
+    right: 155,
+    bottom: 539,
+    fontSize: 20,
+    fontWeight: "bold",
+  },
 
+  TextInputEmail: {
+    backgroundColor: "#C0C0C0",
+    alignSelf: "center",
+    position: "absolute",
+    bottom: 435,
+    height: 40,
+    borderRadius: 10,
+    width: 250,
+  },
 
-    TextInputEmail: {
-        backgroundColor: "#C0C0C0",
-        alignSelf: "center", 
-        position: "absolute",
-        bottom: 435,
-        height: 40,
-        borderRadius: 10,
-        width: 250, 
+  TextInputPassword: {
+    backgroundColor: "#C0C0C0",
+    alignSelf: "center",
+    position: "absolute",
+    bottom: 350,
+    height: 40,
+    borderRadius: 10,
+    width: 250,
+  },
 
-    },
+  inputViewEmail: {
+    border: "1px solid",
+    alignItems: "center",
+    position: "absolute",
+    bottom: 480,
+    right: 277,
+  },
 
-    TextInputPassword: {
-        backgroundColor: "#C0C0C0",
-        alignSelf: "center", 
-        position: "absolute",
-        bottom: 350,
-        height: 40,
-        borderRadius: 10,
-        width: 250, 
+  inputViewPassword: {
+    border: "1px solid",
+    alignItems: "center",
+    position: "absolute",
+    bottom: 395,
+    width: 190,
+  },
 
-    },
+  signupBtn: {
+    backgroundColor: "#2e216f",
+    alignSelf: "center",
+    position: "absolute",
+    bottom: 235,
+    height: 40,
+    borderRadius: 10,
+    width: 250,
+  },
 
-    inputViewEmail: {
-        border: "1px solid",
-        alignItems: "center",   
-        position: "absolute",
-        bottom: 480,   
-        right: 277 
+  loginBtn: {
+    alignSelf: "center",
+    position: "absolute",
+    bottom: 180,
+    height: 40,
+    borderRadius: 10,
+    width: 250,
+    fontWeight: "underline",
+    fontSize: 15,
+  },
 
-    },
+  loginText: {
+    color: "white",
+    alignself: "center",
+    position: "absolute",
+    right: 155,
+    bottom: 12,
+    right: 107,
+    fontWeight: "bold",
+  },
 
-    inputViewPassword: {
-        border: "1px solid",
-        alignItems: "center",   
-        position: "absolute",
-        bottom: 395,    
-        width: 190, 
-    },
-
-    signupBtn:{
-        backgroundColor: "#2e216f",
-        alignSelf: "center", 
-        position: "absolute",
-        bottom: 235,
-        height: 40,
-        borderRadius: 10,
-        width: 250, 
-    },
-
-    loginBtn:{
-        alignSelf: "center", 
-        position: "absolute",
-        bottom: 180,
-        height: 40,
-        borderRadius: 10,
-        width: 250, 
-        fontWeight: "underline",
-        fontSize: 15,
-        
-    },
-
-    loginText:{
-        color: "white",
-        alignself: "center", 
-        position: "absolute",
-        right:155,
-        bottom: 12,
-        right: 107,
-        fontWeight: "bold",
-    },
-
-    orText:{
-        color: "#00bfff",
-        alignSelf: "center", 
-        position: "absolute",
-        bottom: 95,
-        height: 40,
-    }
-
-    
-
-    
+  orText: {
+    color: "#00bfff",
+    alignSelf: "center",
+    position: "absolute",
+    bottom: 95,
+    height: 40,
+  },
 });
