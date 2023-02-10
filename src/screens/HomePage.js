9d04f8f4d73fd075bb7e3520a13fa84c3f24acf8
Anders <andersmyrmel@gmail.com>
2023-02-10 16:33:47 +0100

    add temporary sign out button on homepage

--- src/screens/HomePage.js
+++ src/screens/HomePage.js
@@ -6,11 +6,12 @@ import {
 	StatusBar,
 	ScrollView,
 	ImageBackground,
+	Button,
 } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { styles } from '../styles/HomeStyle';
 import { doc, getDoc } from 'firebase/firestore';
-import { db } from '../../firebaseConfig';
+import { db, auth } from '../../firebaseConfig';
 
 const HomePage = ({ navigation }) => {
 	useEffect(() => {
@@ -31,6 +32,15 @@ const HomePage = ({ navigation }) => {
 
 	const Alert = () => {
 		alert('Kommer når vi har fått fikset profil siden');
+	};
+
+	const handleSignOut = () => {
+		auth
+			.signOut()
+			.then(() => {
+				navigation.replace('loginpage');
+			})
+			.catch((error) => console.log(error.message));
 	};
 
 	return (
@@ -121,6 +131,15 @@ const HomePage = ({ navigation }) => {
 								<Text style={styles.knapptext}>Øving til sert nr3</Text>
 							</ImageBackground>
 						</TouchableOpacity>
+						<View style={styles.loginBtn}>
+							<Button
+								title={'Sign out'}
+								color="white"
+								fontWeight="bold"
+								style={styles.loginBtn}
+								onPress={handleSignOut}
+							/>
+						</View>
 					</View>
 				</ScrollView>
 			</SafeAreaView>
