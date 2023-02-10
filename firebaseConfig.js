a7486e6e047f09925afbfa43102a0e7c492f9f7b
Anders <andersmyrmel@gmail.com>
2023-02-10 15:21:40 +0100

    add auth to firebaseconfig

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
 	apiKey: 'AIzaSyDysqEOPBHzOuHp6P5IXDCy3OxdLJNlc2E',
@@ -11,4 +12,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
 export const db = getFirestore(app);
