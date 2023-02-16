ed766b3776a7c79529c830ab306e65c71afa2321
Anders <andersmyrmel@gmail.com>
2023-02-16 20:53:43 +0100

    add expo splashscreen, edit app.js to keep splash visible until fonts are loaded

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
+import { useCallback } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 import LoginPage from './src/screens/LoginPage';
 import HomePage from './src/screens/HomePage';
 import QuizInfo from './src/screens/QuizInfo';
 import SignUpPage from './src/screens/SignUpPage';
 
+SplashScreen.preventAutoHideAsync();
 const Stack = createNativeStackNavigator();
-const customFonts = {
-	PoppinsBold: require('./src/assets/fonts/Poppins-Bold.ttf'),
-};
 
 export default function App() {
-	const [isLoaded] = useFonts(customFonts);
+	const [fontsLoaded] = useFonts({
+		PoppinsBold: require('./src/assets/fonts/Poppins-Bold.ttf'),
+	});
+
+	const onLayoutRootView = useCallback(async () => {
+		if (fontsLoaded) {
+			await SplashScreen.hideAsync();
+		}
+	}, [fontsLoaded]);
+
+	if (!fontsLoaded) {
+		return null;
+	}
 
 	return (
-		<NavigationContainer>
+		<NavigationContainer onReady={onLayoutRootView}>
 			<Stack.Navigator screenOptions={{ headerShown: false }}>
 				<Stack.Screen name="loginpage" component={LoginPage} />
 				<Stack.Screen name="signuppage" component={SignUpPage} />
