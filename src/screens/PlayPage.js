6f2d9e4237a1d0eeb4c9e679df97f07f34e5ecfe
Anders <andersmyrmel@gmail.com>
2023-02-17 20:51:25 +0100

    modified playpage styling

--- src/screens/PlayPage.js
+++ src/screens/PlayPage.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {
 	Text,
-	View,
 	TouchableOpacity,
 	StatusBar,
 	ImageBackground,
@@ -15,32 +14,40 @@ const PlayPage = ({ navigation }) => {
 			source={require('../assets/images/play_bg.png')}
 			style={{ flex: 1, width: null, alignSelf: 'stretch' }}
 		>
-			<SafeAreaView style={styles.containerTo}>
-				<View>
-					<TouchableOpacity
-						onPress={() => console.log('test')}
-						style={styles.btnBackToHome}
-					>
-						<Text style={styles.knapptext}>X</Text>
-					</TouchableOpacity>
-				</View>
-				<View style={styles.textWrapper}>
-					<Text style={styles.levelText}>Spørsmål 5 av 20</Text>
-					<Text style={styles.Infotext}>
-						Hvilket av disse alternativene er korrekt?
-					</Text>
-				</View>
+			<SafeAreaView>
 				<TouchableOpacity
-					style={styles.startBtn}
+					style={styles.btnBack}
+					onPress={() => navigation.navigate('homepage')}
+				>
+					<Text style={styles.knapptext}>X</Text>
+				</TouchableOpacity>
+				<Text style={styles.IndexText}>Spørsmål 5 av 20</Text>
+				<Text style={styles.QuestionText}>
+					Hvilket av disse alternativene er korrekt?
+				</Text>
+				<TouchableOpacity
+					style={styles.btnChoice}
 					onPress={() => navigation.navigate('homepage')}
 				>
 					<Text style={styles.btnText}>SVAR 1</Text>
 				</TouchableOpacity>
 				<TouchableOpacity
-					style={[styles.startBtn, { margin: 70 }]}
+					style={[styles.btnChoice, { margin: 70 }]}
 					onPress={() => navigation.navigate('homepage')}
 				>
 					<Text style={styles.btnText}>SVAR 2</Text>
+				</TouchableOpacity>
+				<TouchableOpacity
+					style={[styles.btnChoice, { margin: 140 }]}
+					onPress={() => navigation.navigate('homepage')}
+				>
+					<Text style={styles.btnText}>SVAR 3</Text>
+				</TouchableOpacity>
+				<TouchableOpacity
+					style={[styles.btnChoice, { margin: 210 }]}
+					onPress={() => navigation.navigate('homepage')}
+				>
+					<Text style={styles.btnText}>SVAR 4</Text>
 				</TouchableOpacity>
 			</SafeAreaView>
 			<StatusBar translucent backgroundColor="transparent" />
--- src/styles/PlayStyle.js
+++ src/styles/PlayStyle.js
@@ -1,8 +1,14 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-	containerTo: {
-		flex: 1,
+	btnBack: {
+		width: 50,
+		height: 50,
+		borderRadius: 30,
+		position: 'relative',
+		left: 20,
+		top: 10,
+		zIndex: 1,
 	},
 	knapptext: {
 		fontSize: 30,
@@ -11,46 +17,34 @@ export const styles = StyleSheet.create({
 		padding: 5,
 		paddingLeft: 10,
 	},
-	textWrapper: {
-		textAlign: 'center',
-		marginLeft: 'auto',
-		marginRight: 'auto',
-		bottom: 60,
+	IndexText: {
+		fontSize: 17,
+		color: '#000',
+		top: 241,
+		fontWeight: 'bold',
 	},
-	Infotext: {
+	QuestionText: {
 		fontSize: 30,
 		paddingTop: '90%',
 		color: '#000',
 		fontSize: 32,
 		fontWeight: 'bold',
 		marginVertical: 1,
+		bottom: 100,
 	},
-	levelText: {
-		fontSize: 17,
-		color: '#000',
-		top: 341,
-		fontWeight: 'bold',
-	},
-	descriptionText: {
-		fontSize: 15,
-		fontWeight: '600',
-		width: 300,
-		marginVertical: 10,
-	},
-	btnBackToHome: {
+	btnBack: {
 		width: 50,
 		height: 50,
 		borderRadius: 30,
 		position: 'relative',
 		left: 20,
 		top: 10,
-		backgroundColor: 'red',
 	},
-	startBtn: {
+	btnChoice: {
 		backgroundColor: '#3F51B5',
 		alignSelf: 'center',
 		position: 'absolute',
-		top: 530,
+		top: 470,
 		height: 58,
 		borderRadius: 22,
 		width: 314,
