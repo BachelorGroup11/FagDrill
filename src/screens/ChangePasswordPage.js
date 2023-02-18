26ebe04859ce85525a4caa1ec9e75212d8fab107
Shaheen Thayalan <shaheen@Shaheens-MBP.uia.no>
2023-02-18 01:00:40 +0100

    The User view has been updated, the buttons in the user page directs to the right places but has some difficulties pressing the button. This will be fixed and also the main view for progress and change password

---
+++ src/screens/ChangePasswordPage.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from "react";
+import { Text, View, TextInput, Button, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { styles } from "../styles/ProgressStyle";
+
+const ChangePasswordPage = ({ navigation }) => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Need Fixing</Text>
+
+      <View style={styles.container}>
+        <TouchableOpacity
+          style={styles.btnBackToHome}
+          onPress={() => navigation.navigate("userpage")}
+        >
+          <Text style={styles.knapptext}>X</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default ChangePasswordPage;
--- src/screens/UserPage.js
+++ src/screens/UserPage.js
@@ -2,8 +2,19 @@ import React, { useState, useEffect } from "react";
 import { Text, View, TextInput, Button, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { styles } from "../styles/UserStyle";
+import { collection, getDocs } from "firebase/firestore";
+import { db, auth } from "../../firebaseConfig";
 
 const UserPage = ({ navigation, onPress }) => {
+  const handleSignOut = () => {
+    auth
+      .signOut()
+      .then(() => {
+        navigation.replace("loginpage");
+      })
+      .catch((error) => console.log(error));
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>Your Account</Text>
@@ -18,19 +29,19 @@ const UserPage = ({ navigation, onPress }) => {
 
         <TouchableOpacity
           style={styles.appButtonContainer1}
-          onPress={onPress}
+          onPress={() => navigation.navigate("progresspage")}
         />
         <Text style={styles.YourAccountText1}>Progress</Text>
         <TouchableOpacity
           style={styles.appButtonContainer1}
-          onPress={onPress}
+          onPress={() => navigation.navigate("changepasswordpage")}
         />
         <Text style={styles.YourAccountText1}>Change Password</Text>
         <TouchableOpacity
           style={styles.appButtonContainer2}
-          onPress={() => navigation.navigate("loginpage")}
+          onPress={() => handleSignOut()}
         />
-        <Text style={styles.YourAccountText2}>Log Out</Text>
+        <Text style={styles.YourAccountText2}>Sign Out</Text>
       </View>
     </SafeAreaView>
   );
