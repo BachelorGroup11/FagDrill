eb1d3ea47a5cb47986c34580ee6ce0964d0d901a
Anders <andersmyrmel@gmail.com>
2023-02-20 23:39:49 +0100

    display result upon completed quiz

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,17 +1,14 @@
 import { styles } from '../styles/components/OptionStyle';
 import { Text, TouchableOpacity, View } from 'react-native';
 import { updateQuiz } from '../utilities/updateQuiz';
-import { useNavigation } from '@react-navigation/native';
 
 // Component responsible for rendering buttons with answer options for a given question
 export const Option = ({ value, id, state, dispatch }) => {
-	const navigation = useNavigation();
-
 	return (
 		<View>
 			<TouchableOpacity
 				style={styles.btnChoice}
-				onPress={() => updateQuiz(navigation, id, state, dispatch)}
+				onPress={() => updateQuiz(id, state, dispatch)}
 			>
 				<Text style={styles.btnText}>{value}</Text>
 			</TouchableOpacity>
