31de6c48c3a5d821ce2f8f81e48084b1d991b9dc
Anders <andersmyrmel@gmail.com>
2023-02-20 19:38:37 +0100

    extract homepage buttons to seperate component

---
+++ src/styles/components/GoToQuizStyle.js
@@ -0,0 +1,33 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+	knapptext: {
+		fontSize: 30,
+		alignSelf: 'center',
+		color: '#ffffff',
+
+		padding: 5,
+		paddingLeft: 10,
+	},
+	knappBytteS: {
+		borderRadius: 22,
+		overflow: 'hidden',
+		borderColor: '#ffffff',
+		alignSelf: 'center',
+		justifyContent: 'center',
+		marginVertical: 10,
+		marginHorizontal: 0,
+		height: 200,
+		width: 320,
+		borderColor: '#2e216f',
+		borderWidth: 0,
+	},
+	imgButton: {
+		flex: 1,
+		width: '100%',
+		alignSelf: 'stretch',
+		height: '100%',
+		justifyContent: 'center',
+		overflow: 'hidden',
+	},
+});
