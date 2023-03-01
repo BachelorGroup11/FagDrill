1b43916639635f861c423dd715e44392c50c2b2f
Shaheen Thayalan <shaheen@Shaheens-MBP.uia.no>
2023-03-01 15:32:10 +0100

    Test 4

--- src/screens/ChangePasswordPage.js
+++ src/screens/ChangePasswordPage.js
@@ -1,12 +1,54 @@
 import React, { useState, useEffect } from "react";
-import { Text, View, TextInput, Button, TouchableOpacity } from "react-native";
+import {
+  Text,
+  View,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { styles } from "../styles/ProgressStyle";
+import { styles } from "../styles/ChangePasswordStyle";
+import { collection, getDocs } from "firebase/firestore";
+import { db, auth } from "../../firebaseConfig";
+import {
+  getAuth,
+  updatePassword,
+  reauthenticateWithCredential,
+} from "firebase/auth";
 
 const ChangePasswordPage = ({ navigation }) => {
+  const [currentPassword, setCurrentPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+
+  reauthenticate = (currentPassword) => {
+    const auth = getAuth();
+    const user = auth.currentUser;
+    const cred = auth.EmailAuthProcider.credential(user.email, currentPassword);
+    return user.reathenticateWithCredential(cred);
+  };
+
+  const handleUpdatePassowrd = () => {
+    reauthenticate(currentPassword)
+      .then(() => {
+        var user = auth.currentUser;
+        user
+          .updatePassword(user, newPassword)
+          .then(() => {
+            Alert.alert("Password was changed");
+          })
+          .catch((error) => {
+            Alert.alert(error.messsage);
+          });
+      })
+      .catch((error) => {
+        Alert.alert(error.messsage);
+      });
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>Need Fixing</Text>
+      <Text style={styles.title}>Change Password</Text>
 
       <View style={styles.container}>
         <TouchableOpacity
@@ -14,6 +56,30 @@ const ChangePasswordPage = ({ navigation }) => {
           onPress={() => navigation.navigate("userpage")}
         >
           <Text style={styles.knapptext}>X</Text>
+        </TouchableOpacity>
+        <View style={styles.inputViewEmail}>
+          <Text>Current Password</Text>
+        </View>
+        <TextInput
+          style={styles.TextInputEmail}
+          onChangeText={() => setCurrentPassword()}
+          secureTextEntry
+        />
+
+        <View style={styles.inputViewPassword}>
+          <Text>New Password</Text>
+        </View>
+        <TextInput
+          style={styles.TextInputPassword}
+          onChangeText={() => setNewPassword()}
+          secureTextEntry
+        />
+
+        <TouchableOpacity
+          style={styles.appButtonContainer2}
+          onPress={() => handleUpdatePassowrd()}
+        >
+          <Text>Change Password</Text>
         </TouchableOpacity>
       </View>
     </SafeAreaView>
--- src/styles/ChangePasswordStyle.js
+++ src/styles/ChangePasswordStyle.js
@@ -6,12 +6,80 @@ export const styles = StyleSheet.create({
   },
 
   btnBackToHome: {
-    width: 50,
-    height: 50,
+    width: 42,
+    height: 42,
     borderRadius: 30,
     position: "relative",
     left: 300,
     top: 40,
     fontSize: 32,
+  },
+
+  title: {
+    alignself: "center",
+    position: "absolute",
+    right: 110,
+    bottom: 539,
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+
+  TextInputEmail: {
+    backgroundColor: "#C0C0C0",
+    alignSelf: "center",
+    position: "absolute",
+    bottom: 375,
+    height: 40,
+    borderRadius: 10,
+    width: 250,
+  },
+
+  TextInputPassword: {
+    backgroundColor: "#C0C0C0",
+    alignSelf: "center",
+    position: "absolute",
+    bottom: 270,
+    height: 40,
+    borderRadius: 10,
+    width: 250,
+  },
+
+  inputViewEmail: {
+    border: "1px solid",
+    alignItems: "center",
+    position: "absolute",
+    bottom: 420,
+    right: 198,
+  },
+
+  inputViewPassword: {
+    border: "1px solid",
+    alignItems: "center",
+    position: "absolute",
+    bottom: 315,
+    width: 190,
+    right: 170,
+  },
+
+  loginBtn: {
+    backgroundColor: "#3F51B5",
+    alignSelf: "center",
+    position: "absolute",
+    bottom: 235,
+    height: 40,
+    borderRadius: 10,
+    width: 250,
+
+    appButtonContainer2: {
+      elevation: 8,
+      backgroundColor: "#3F51B5",
+      borderRadius: 22,
+      paddingVertical: 10,
+      paddingHorizontal: 12,
+      bottom: -300,
+      width: 310,
+      height: 60,
+      right: -35,
+    },
   },
 });
