d9845c3cd7627b67074086192208c609e313d058
Anders <andersmyrmel@gmail.com>
2023-03-01 15:48:33 +0100

    fix updatepassword func

--- src/screens/ChangePasswordPage.js
+++ src/screens/ChangePasswordPage.js
@@ -1,89 +1,77 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import {
-  Text,
-  View,
-  TextInput,
-  Button,
-  TouchableOpacity,
-  Alert,
-} from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { styles } from "../styles/ChangePasswordStyle";
-import { collection, getDocs } from "firebase/firestore";
-import { db, auth } from "../../firebaseConfig";
+	Text,
+	View,
+	TextInput,
+	Button,
+	TouchableOpacity,
+	Alert,
+} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { styles } from '../styles/ChangePasswordStyle';
+import { collection, getDocs } from 'firebase/firestore';
+import { db, auth } from '../../firebaseConfig';
 import {
-  getAuth,
-  updatePassword,
-  reauthenticateWithCredential,
-} from "firebase/auth";
+	getAuth,
+	updatePassword,
+	reauthenticateWithCredential,
+} from 'firebase/auth';
 
 const ChangePasswordPage = ({ navigation }) => {
-  const [currentPassword, setCurrentPassword] = useState("");
-  const [newPassword, setNewPassword] = useState("");
+	const [currentPassword, setCurrentPassword] = useState('');
+	const [newPassword, setNewPassword] = useState('');
 
-  reauthenticate = (currentPassword) => {
-    const auth = getAuth();
-    const user = auth.currentUser;
-    const cred = auth.EmailAuthProcider.credential(user.email, currentPassword);
-    return user.reathenticateWithCredential(cred);
-  };
+	const changePassword = (newPassword) => {
+		const auth = getAuth();
+		const user = auth.currentUser;
 
-  const handleUpdatePassowrd = () => {
-    reauthenticate(currentPassword)
-      .then(() => {
-        var user = auth.currentUser;
-        user
-          .updatePassword(user, newPassword)
-          .then(() => {
-            Alert.alert("Password was changed");
-          })
-          .catch((error) => {
-            Alert.alert(error.messsage);
-          });
-      })
-      .catch((error) => {
-        Alert.alert(error.messsage);
-      });
-  };
+		updatePassword(user, newPassword)
+			.then(() => {
+				console.log(`Set new password to: ${newPassword}`);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>Change Password</Text>
+	return (
+		<SafeAreaView style={styles.container}>
+			<Text style={styles.title}>Change Password</Text>
 
-      <View style={styles.container}>
-        <TouchableOpacity
-          style={styles.btnBackToHome}
-          onPress={() => navigation.navigate("userpage")}
-        >
-          <Text style={styles.knapptext}>X</Text>
-        </TouchableOpacity>
-        <View style={styles.inputViewEmail}>
-          <Text>Current Password</Text>
-        </View>
-        <TextInput
-          style={styles.TextInputEmail}
-          onChangeText={() => setCurrentPassword()}
-          secureTextEntry
-        />
+			<View style={styles.container}>
+				<TouchableOpacity
+					style={styles.btnBackToHome}
+					onPress={() => navigation.navigate('userpage')}
+				>
+					<Text style={styles.knapptext}>X</Text>
+				</TouchableOpacity>
+				<View style={styles.inputViewEmail}>
+					<Text>Current Password</Text>
+				</View>
+				<TextInput
+					style={styles.TextInputEmail}
+					onChangeText={(text) => setCurrentPassword(text)}
+					secureTextEntry
+				/>
 
-        <View style={styles.inputViewPassword}>
-          <Text>New Password</Text>
-        </View>
-        <TextInput
-          style={styles.TextInputPassword}
-          onChangeText={() => setNewPassword()}
-          secureTextEntry
-        />
+				<View style={styles.inputViewPassword}>
+					<Text>New Password</Text>
+				</View>
+				<TextInput
+					style={styles.TextInputPassword}
+					onChangeText={(text) => setNewPassword(text)}
+					secureTextEntry
+				/>
 
-        <TouchableOpacity
-          style={styles.appButtonContainer2}
-          onPress={() => handleUpdatePassowrd()}
-        >
-          <Text>Change Password</Text>
-        </TouchableOpacity>
-      </View>
-    </SafeAreaView>
-  );
+				<TouchableOpacity
+					style={styles.appButtonContainer2}
+					onPress={() => changePassword(newPassword)}
+				>
+					<Text>Change Password</Text>
+				</TouchableOpacity>
+			</View>
+		</SafeAreaView>
+	);
 };
 
 export default ChangePasswordPage;
--- src/screens/UserPage.js
+++ src/screens/UserPage.js
@@ -1,50 +1,53 @@
-import React, { useState, useEffect } from "react";
-import { Text, View, TextInput, Button, TouchableOpacity } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { styles } from "../styles/UserStyle";
-import { collection, getDocs } from "firebase/firestore";
-import { db, auth } from "../../firebaseConfig";
+import React, { useState, useEffect } from 'react';
+import { Text, View, TextInput, Button, TouchableOpacity } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { styles } from '../styles/UserStyle';
+import { collection, getDocs } from 'firebase/firestore';
+import { db, auth } from '../../firebaseConfig';
 
 const UserPage = ({ navigation, onPress }) => {
-  const handleSignOut = () => {
-    auth
-      .signOut()
-      .then(() => {
-        navigation.replace("loginpage");
-      })
-      .catch((error) => console.log(error));
-  };
+	const handleSignOut = () => {
+		auth
+			.signOut()
+			.then(() => {
+				navigation.replace('loginpage');
+			})
+			.catch((error) => console.log(error));
+	};
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>Your Account</Text>
+	return (
+		<SafeAreaView style={styles.container}>
+			<Text style={styles.title}>Your Account</Text>
 
-      <View style={styles.container}>
-        <TouchableOpacity
-          style={styles.btnBackToHome}
-          onPress={() => navigation.navigate("homepage")}
-        >
-          <Text style={styles.knapptext}>X</Text>
-        </TouchableOpacity>
+			<View style={styles.container}>
+				<TouchableOpacity
+					style={styles.btnBackToHome}
+					onPress={() => navigation.navigate('homepage')}
+				>
+					<Text style={styles.knapptext}>X</Text>
+				</TouchableOpacity>
 
-        <TouchableOpacity
-          style={styles.appButtonContainer1}
-          onPress={() => navigation.navigate("progresspage")}
-        />
-        <Text style={styles.YourAccountText1}>Progress</Text>
-        <TouchableOpacity
-          style={styles.appButtonContainer1}
-          onPress={() => navigation.navigate("changepasswordpage")}
-        />
-        <Text style={styles.YourAccountText1}>Change Password</Text>
-        <TouchableOpacity
-          style={styles.appButtonContainer2}
-          onPress={() => handleSignOut()}
-        />
-        <Text style={styles.YourAccountText2}>Sign Out</Text>
-      </View>
-    </SafeAreaView>
-  );
+				<TouchableOpacity
+					style={styles.appButtonContainer1}
+					onPress={() => navigation.navigate('progresspage')}
+				/>
+				<Text style={styles.YourAccountText1}>Progress</Text>
+
+				<TouchableOpacity
+					style={styles.appButtonContainer1}
+					onPress={() => navigation.navigate('changepasswordpage')}
+				>
+					<Text style={styles.YourAccountText1}>Change Password</Text>
+				</TouchableOpacity>
+
+				<TouchableOpacity
+					style={styles.appButtonContainer2}
+					onPress={() => handleSignOut()}
+				/>
+				<Text style={styles.YourAccountText2}>Sign Out</Text>
+			</View>
+		</SafeAreaView>
+	);
 };
 
 export default UserPage;
