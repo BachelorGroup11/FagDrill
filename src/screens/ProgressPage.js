7e5a522f4b2a223dd9cd360c3ec680e5a8ecf08c
Anders <andersmyrmel@gmail.com>
2023-03-17 16:04:44 +0100

    factor code post merge

--- src/screens/ProgressPage.js
+++ src/screens/ProgressPage.js
@@ -1,23 +1,21 @@
-import React, { useState, useEffect } from "react";
-import { Text, View, TextInput, Button, TouchableOpacity } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { styles } from "../styles/ProgressStyle";
+import { Text, View, TouchableOpacity } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { styles } from '../styles/screens/ProgressStyle';
 
 const ProgressPage = ({ navigation }) => {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>Need Fixing</Text>
-
-      <View style={styles.container}>
-        <TouchableOpacity
-          style={styles.btnBackToHome}
-          onPress={() => navigation.navigate("userpage")}
-        >
-          <Text style={styles.knapptext}>X</Text>
-        </TouchableOpacity>
-      </View>
-    </SafeAreaView>
-  );
+	return (
+		<SafeAreaView style={styles.container}>
+			<Text style={styles.title}>Need Fixing</Text>
+			<View style={styles.container}>
+				<TouchableOpacity
+					style={styles.btnBackToHome}
+					onPress={() => navigation.navigate('userpage')}
+				>
+					<Text style={styles.knapptext}>X</Text>
+				</TouchableOpacity>
+			</View>
+		</SafeAreaView>
+	);
 };
 
 export default ProgressPage;
