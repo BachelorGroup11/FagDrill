9eb283386619ac6d4427a3c8efb9114ab7c361d9
Anders <andersmyrmel@gmail.com>
2023-03-20 18:14:09 +0100

    add instant feedback whether answer was correct or incorrect

--- src/components/Option.js
+++ src/components/Option.js
@@ -3,12 +3,12 @@ import { Text, TouchableOpacity, View } from 'react-native';
 import { updateQuiz } from '../utilities/updateQuiz';
 
 // Component responsible for rendering buttons with answer options for a given question
-export const Option = ({ value, id, state, dispatch }) => {
+export const Option = ({ value, id, state, dispatch, style }) => {
 	return (
 		<View>
 			<TouchableOpacity
-				style={styles.btnChoice}
-				onPress={() => updateQuiz(id, state, dispatch)}
+				style={[styles.btnChoice, style]}
+				onPress={() => state.selected == -1 && updateQuiz(id, state, dispatch)}
 			>
 				<Text style={styles.btnText}>{value}</Text>
 			</TouchableOpacity>
--- src/screens/PlayPage.js
+++ src/screens/PlayPage.js
@@ -8,38 +8,62 @@ import {
 } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { styles } from '../styles/screens/PlayStyle';
+import { db } from '../../firebaseConfig';
+import { getAuth } from 'firebase/auth';
+import { addDoc, collection, Timestamp } from 'firebase/firestore';
 import ProgressBar from 'react-native-progress/Bar';
 import { Option, GoBack, LoadingAnimation } from '../components/Index';
 import { QuizReducer, INITIAL_STATE } from '../utilities/QuizReducer';
 import { fetchQuiz } from '../utilities/fetchQuiz';
 
 const PlayPage = ({ route, navigation }) => {
+	const { quiz } = route.params;
 	// Contains all relevant information on the specified quiz, see: ../utilities/QuizReducer
 	const [state, dispatch] = useReducer(QuizReducer, INITIAL_STATE);
 
 	// Retrieve all questions from specified quiz then set state with information on first render
 	useEffect(() => {
-		const { quiz } = route.params;
 		fetchQuiz(quiz, dispatch).then(() =>
 			dispatch({ type: 'setisloading', payload: false })
 		);
 	}, []);
 
 	// Continue to next question on answer input
-	useEffect(() => {
+	const nextQuestion = () => {
 		try {
 			dispatch({
 				type: 'setmulitple',
 				payload: {
+					selected: -1,
 					questionText: state.questionsArray[state.index].question_text,
 					options: state.questionsArray[state.index].options,
 					correctOption: state.questionsArray[state.index].correct_answer,
 				},
 			});
 		} catch (error) {
-			console.log(error);
+			dispatch({
+				type: 'setindex',
+				payload: state.index + 1,
+			});
 		}
-	}, [state.index]);
+	};
+
+	// Create new document in results collection and push result
+	const finishQuiz = async () => {
+		const auth = getAuth();
+		const user = auth.currentUser;
+
+		await addDoc(collection(db, 'results'), {
+			name: `Quiz ${route.params.number}`,
+			quiz_id: quiz,
+			score: state.score,
+			total_questions: state.quizLength,
+			user_id: user.uid,
+			attempt: 1,
+			date: Timestamp.now(),
+		});
+		navigation.navigate('resultspage');
+	};
 
 	return (
 		<View style={styles.containerTo}>
@@ -52,15 +76,29 @@ const PlayPage = ({ route, navigation }) => {
 				>
 					<SafeAreaView>
 						<GoBack nav={navigation} destination={'homepage'} />
-						<ProgressBar
-							progress={state.index / state.quizLength}
-							style={styles.progressbar}
-							width={300}
-							height={20}
-							borderRadius={20}
-							color={'#3F51B5'}
-						/>
-						{state.index < state.quizLength ? (
+						<View style={styles.progressContainer}>
+							<ProgressBar
+								progress={state.index / state.quizLength}
+								style={styles.progressbar}
+								width={260}
+								height={20}
+								borderRadius={30}
+								color={'#3F51B5'}
+							/>
+						</View>
+						{state.index > state.quizLength ? (
+							<View>
+								<Text style={styles.QuestionText}>
+									Du har fullført quizen. Gå videre for å se resultatene dine.
+								</Text>
+								<TouchableOpacity
+									style={styles.resultsBtn}
+									onPress={finishQuiz}
+								>
+									<Text style={styles.btnText}>Resultater</Text>
+								</TouchableOpacity>
+							</View>
+						) : (
 							<View>
 								<Text style={styles.QuestionText}>{state.questionText}</Text>
 								{state.options.map((option, idx) => (
@@ -70,20 +108,30 @@ const PlayPage = ({ route, navigation }) => {
 										id={idx}
 										state={state}
 										dispatch={dispatch}
+										style={
+											idx === state.selected
+												? idx === state.correctOption
+													? {
+															borderWidth: 5,
+															borderColor: '#00FFE0',
+													  }
+													: { borderWidth: 5, borderColor: 'red' }
+												: idx === state.correctOption &&
+												  state.selected != -1 && {
+														borderWidth: 5,
+														borderColor: '#00FFE0',
+												  }
+										}
 									/>
 								))}
-							</View>
-						) : (
-							<View>
-								<Text style={styles.QuestionText}>
-									Du har fullført quizen. Gå videre for å se resultatene dine.
-								</Text>
-								<TouchableOpacity
-									style={styles.resultsBtn}
-									onPress={() => navigation.navigate('resultspage')}
-								>
-									<Text style={styles.btnText}>Resultater</Text>
-								</TouchableOpacity>
+								{state.selected != -1 && (
+									<TouchableOpacity
+										style={styles.nextBtn}
+										onPress={nextQuestion}
+									>
+										<Text style={[styles.btnText, { fontSize: 22 }]}>Next</Text>
+									</TouchableOpacity>
+								)}
 							</View>
 						)}
 					</SafeAreaView>
