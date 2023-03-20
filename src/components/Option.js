9a481cf9289867ed6f1af4a31d34a87642a22906
Anders <andersmyrmel@gmail.com>
2023-03-20 20:49:35 +0100

    rm redundant code

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,14 +1,24 @@
 import { Text, TouchableOpacity, View } from 'react-native';
 import { styles } from '../styles/components/OptionStyle';
-import { updateQuiz } from '../utilities/updateQuiz';
 
 // Component responsible for rendering buttons with answer options for a given question
 export const Option = ({ value, id, state, dispatch, style }) => {
+	const updateQuiz = () => {
+		dispatch({
+			type: 'setmulitple',
+			payload: {
+				selected: id,
+				index: state.index + 1,
+				score: id == state.correctOption ? state.score + 1 : state.score,
+			},
+		});
+	};
+
 	return (
 		<View>
 			<TouchableOpacity
 				style={[styles.btnChoice, style]}
-				onPress={() => state.selected === -1 && updateQuiz(id, state, dispatch)}
+				onPress={() => state.selected === -1 && updateQuiz()}
 			>
 				<Text style={styles.btnText}>{value}</Text>
 			</TouchableOpacity>
