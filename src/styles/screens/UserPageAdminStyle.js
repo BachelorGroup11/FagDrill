07e82d49e40de28a0582fd58fc9a2271c8d8f688
Shaheen Thayalan <shaheen@Shaheens-MBP.uia.no>
2023-03-21 21:00:44 +0100

    Added all the functionality userpage has

--- src/styles/screens/UserPageAdminStyle.js
+++ src/styles/screens/UserPageAdminStyle.js
@@ -1,96 +1,99 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-	container: {
-		paddingTop: 35,
-		flex: 1,
-	},
-	containerTo: {
-		flex: 1,
-	},
-	containerthre: {
-		flex: 1,
-		marginTop: 170,
-		marginBottom: 50,
-	},
-	letsplay: {
-		position: 'absolute',
-		width: '100%',
-		height: 42,
-		left: 32,
-		top: 44,
+  container: {
+    paddingTop: 35,
+    flex: 1,
+  },
+  containerTo: {
+    flex: 1,
+  },
+  containerthre: {
+    flex: 1,
+    marginTop: 170,
+    marginBottom: 50,
+  },
+  letsplay: {
+    position: "absolute",
+    width: "100%",
+    height: 42,
+    left: 32,
+    top: 44,
 
-		fontSize: 30,
-		fontWeight: 'bold',
-		color: '#000000',
-	},
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "#000000",
+  },
 
-	knapptext: {
-		fontSize: 30,
-		alignSelf: 'center',
-		color: '#000',
+  knapptext: {
+    fontSize: 30,
+    alignSelf: "center",
+    color: "#000",
+    fontWeight: "bold",
 
-		padding: 5,
-		paddingLeft: 10,
-	},
+    padding: 5,
+    paddingLeft: 10,
+  },
 
-    Userpagebtn: {
-        backgroundColor: "#3F51B5",
-        alignSelf: 'center',
-        height: 60,
-        borderRadius: 20,
-        width: 350,
-        marginTop: 50,
-    },
-    
-    Userpagebtntext: {
-		fontSize: 30,
-		alignSelf: 'center',
-		color: '#ffff',
+  Userpagebtn: {
+    backgroundColor: "#3F51B5",
+    alignSelf: "center",
+    height: 60,
+    borderRadius: 20,
+    width: 350,
+    marginTop: 50,
+    bottom: -75,
+  },
 
-		padding: 5,
-		paddingLeft: 10,
-	},
+  Userpagebtntext: {
+    fontSize: 30,
+    alignSelf: "center",
+    color: "#ffff",
+    fontWeight: "bold",
 
-	knappBytteS: {
-		borderRadius: 22,
-		overflow: 'hidden',
-		borderColor: '#ffffff',
-		alignSelf: 'center',
-		justifyContent: 'center',
+    padding: 5,
+    paddingLeft: 10,
+  },
 
-		marginVertical: 10,
-		marginHorizontal: 0,
-		height: 200,
-		width: 320,
+  knappBytteS: {
+    borderRadius: 22,
+    overflow: "hidden",
+    borderColor: "#ffffff",
+    alignSelf: "center",
+    justifyContent: "center",
 
-		borderColor: '#2e216f',
-		borderWidth: 0,
-	},
+    marginVertical: 10,
+    marginHorizontal: 0,
+    height: 200,
+    width: 320,
 
-	imgButton: {
-		flex: 1,
-		width: '100%',
-		alignSelf: 'stretch',
-		height: '100%',
-		justifyContent: 'center',
-		overflow: 'hidden',
-	},
-	imgBtn_profile: {
-		position: 'relative',
-		width: 50,
-		height: 50,
-		left: 304,
-		top: 44,
-		borderRadius: 22,
-	},
-	loginBtn: {
-		backgroundColor: '#2e216f',
-		alignSelf: 'center',
-		position: 'relative',
-		height: 40,
-		borderRadius: 10,
-		width: 250,
-		margin: 10,
-	},
+    borderColor: "#2e216f",
+    borderWidth: 0,
+  },
+
+  imgButton: {
+    flex: 1,
+    width: "100%",
+    alignSelf: "stretch",
+    height: "100%",
+    justifyContent: "center",
+    overflow: "hidden",
+  },
+  imgBtn_profile: {
+    position: "relative",
+    width: 50,
+    height: 50,
+    left: 304,
+    top: 44,
+    borderRadius: 22,
+  },
+  loginBtn: {
+    backgroundColor: "#2e216f",
+    alignSelf: "center",
+    position: "relative",
+    height: 40,
+    borderRadius: 10,
+    width: 250,
+    margin: 10,
+  },
 });
