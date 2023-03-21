0e0021a09dcf9dcdb9eace776b50b8df91d37b6b
Shaheen Thayalan <shaheen@Shaheens-MBP.uia.no>
2023-03-21 21:00:44 +0100

    Added all the functionality userpage has

--- src/screens/UserPage_Admin.js
+++ src/screens/UserPage_Admin.js
@@ -1,76 +1,87 @@
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
 import {
-	Text,
-	View,
-	TouchableOpacity,
-	StatusBar,
-	ImageBackground,
-} from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { styles } from '../styles/screens/UserPageAdminStyle';
-import { collection, query, getDocs, where } from 'firebase/firestore';
-import { db } from '../../firebaseConfig';
-import { useEffect } from 'react';
-import { getAuth } from 'firebase/auth';
+  Text,
+  View,
+  TouchableOpacity,
+  StatusBar,
+  ImageBackground,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { styles } from "../styles/screens/UserPageAdminStyle";
+import { getAuth } from "firebase/auth";
 
 const UserPage_Admin = ({ navigation }) => {
-	// temp variabler, sikkert ikke beste metode å plassere de her
-	const auth = getAuth();
-	const user = auth.currentUser;
+  // temp variabler, sikkert ikke beste metode å plassere de her
+  const auth = getAuth();
+  const user = auth.currentUser;
 
-	
+  const goToHome = () => {
+    navigation.navigate("homepage");
+  };
 
-	const goToHome = () => {
-		navigation.navigate('homepage');
-	};
+  const handleSignOut = () => {
+    auth
+      .signOut()
+      .then(() => {
+        navigation.replace("loginpage");
+      })
+      .catch((error) => console.log(error));
+  };
 
-	const handleSignOut = () => {
-		auth
-			.signOut()
-			.then(() => {
-				navigation.replace('loginpage');
-			})
-			.catch((error) => console.log(error));
-	};
+  const goToChangePassword = () => {
+    navigation.navigate("changepasswordpage");
+  };
 
-	return (
-		<ImageBackground
-			source={require('../assets/images/Quizinfo_bg.png')}
-			style={{ flex: 1, width: null, alignSelf: 'stretch' }}
-		>
-			<SafeAreaView style={styles.containerTo}>
-				<View>
-					{user && <Text style={styles.letsplay}>{user.email}</Text>}
-				</View>
-				<TouchableOpacity
-					style={styles.imgBtn_profile}
-					onPress={() => goToHome()}
-				>
-					<Text style={styles.knapptext}>X</Text>
-				</TouchableOpacity>
+  const goToProgress = () => {
+    navigation.navigate("progresspage");
+  };
 
-				<TouchableOpacity style={styles.Userpagebtn}>
-					<Text style={styles.Userpagebtntext}>Administrer quizer</Text>
-				</TouchableOpacity>
+  return (
+    <ImageBackground
+      source={require("../assets/images/Quizinfo_bg.png")}
+      style={{ flex: 1, width: null, alignSelf: "stretch" }}
+    >
+      <SafeAreaView style={styles.containerTo}>
+        <View>{user && <Text style={styles.letsplay}>{user.email}</Text>}</View>
+        <TouchableOpacity
+          style={styles.imgBtn_profile}
+          onPress={() => goToHome()}
+        >
+          <Text style={styles.knapptext}>X</Text>
+        </TouchableOpacity>
 
-				<TouchableOpacity style={styles.Userpagebtn}>
-					<Text style={styles.Userpagebtntext}>Administrer Brukere</Text>
-				</TouchableOpacity>
+        <TouchableOpacity style={styles.Userpagebtn}>
+          <Text style={styles.Userpagebtntext}>Manage Quizzes</Text>
+        </TouchableOpacity>
 
-				<TouchableOpacity style={styles.Userpagebtn}>
-					<Text style={styles.Userpagebtntext}>Quiz resulteter</Text>
-				</TouchableOpacity>
+        <TouchableOpacity style={styles.Userpagebtn}>
+          <Text style={styles.Userpagebtntext}>Manage Users</Text>
+        </TouchableOpacity>
 
-				<TouchableOpacity
-					style={styles.Userpagebtn}
-					onPress={() => handleSignOut()}
-				>
-					<Text style={styles.Userpagebtntext}>Logg ut</Text>
-				</TouchableOpacity>
-			</SafeAreaView>
-			<StatusBar translucent backgroundColor="transparent" />
-		</ImageBackground>
-	);
+        <TouchableOpacity
+          style={styles.Userpagebtn}
+          onPress={() => goToProgress()}
+        >
+          <Text style={styles.Userpagebtntext}>Progress</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.Userpagebtn}
+          onPress={() => goToChangePassword()}
+        >
+          <Text style={styles.Userpagebtntext}>Change Password</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.Userpagebtn}
+          onPress={() => handleSignOut()}
+        >
+          <Text style={styles.Userpagebtntext}>Sign Out</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+      <StatusBar translucent backgroundColor="transparent" />
+    </ImageBackground>
+  );
 };
 
 export default UserPage_Admin;
