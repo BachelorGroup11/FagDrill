3e7cf943609ce68da2a3bc404399d15c1248258f
Shaheen Thayalan <shaheen@Shaheens-MBP.uia.no>
2023-04-04 17:31:04 +0200

    Border around the result fixed. Above 80% green, above 50% yellow, bellow 50% red.

--- src/styles/screens/ResultsStyle.js
+++ src/styles/screens/ResultsStyle.js
@@ -1,18 +1,18 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-	container: {
-		paddingTop: 35,
-		flex: 1,
-	},
-	containerTo: {
-		flex: 1,
-	},
-	resultaterText: {
-		fontSize: 32,
-		fontFamily: 'PoppinsBold',
-		position: 'absolute',
-		top: 24,
-		left: 32,
-	},
+  container: {
+    paddingTop: 35,
+    flex: 1,
+  },
+  containerTo: {
+    flex: 1,
+  },
+  resultaterText: {
+    fontSize: 32,
+    fontFamily: "PoppinsBold",
+    position: "absolute",
+    top: 24,
+    left: 32,
+  },
 });
