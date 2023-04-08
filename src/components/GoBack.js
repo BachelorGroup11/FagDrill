e74cff1eda7c757ad8ca5213bea21ff0452bef2f
Anders Farstad Myrmel <andersfm@uia.no>
2023-04-08 13:10:19 +0200

    change nav to goback instead of destination

--- src/components/GoBack.js
+++ src/components/GoBack.js
@@ -1,15 +1,12 @@
-import { styles } from '../styles/components/GoBackStyle';
-import { Text, TouchableOpacity, View } from 'react-native';
+import { styles } from "../styles/components/GoBackStyle";
+import { Text, TouchableOpacity, View } from "react-native";
 
 // Component responsible for rendering navigate back symbol in top left corner of application, specify which screen to navigate to as prop
 export const GoBack = ({ nav, destination }) => {
 	return (
 		// Set zIndex to 1 to ensure component is always pressable
 		<View style={{ zIndex: 1 }}>
-			<TouchableOpacity
-				style={styles.btnBack}
-				onPress={() => nav.navigate(destination)}
-			>
+			<TouchableOpacity style={styles.btnBack} onPress={() => nav.goBack()}>
 				<Text style={styles.knapptext}>X</Text>
 			</TouchableOpacity>
 		</View>
