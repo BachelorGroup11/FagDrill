fcfc080108921fd98f2384301832ea9e16d0d826
Anders <andersmyrmel@gmail.com>
2023-04-17 15:16:16 +0200

    add duration to edit and create quiz

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,26 +1,27 @@
-import { View, Text, TouchableOpacity, Alert } from "react-native";
-import { styles } from "../styles/components/QuizStyle";
-import { db } from "../../firebaseConfig";
-import { doc, deleteDoc } from "firebase/firestore";
-import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { View, Text, TouchableOpacity, Alert } from 'react-native';
+import { styles } from '../styles/components/QuizStyle';
+import { db } from '../../firebaseConfig';
+import { doc, deleteDoc } from 'firebase/firestore';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 export const Quiz = ({
 	id,
 	navigation,
 	name,
 	description,
+	duration,
 	users,
 	questions,
 	quizzes,
 	setQuizzes,
 }) => {
 	const deleteQuiz = () => {
-		return Alert.alert("", `Are you sure you wish to delete ${name}?`, [
-			{ text: "Cancel" },
+		return Alert.alert('', `Are you sure you wish to delete ${name}?`, [
+			{ text: 'Cancel' },
 			{
-				text: "Confirm",
+				text: 'Confirm',
 				onPress: async () => {
-					await deleteDoc(doc(db, "quizzes", id));
+					await deleteDoc(doc(db, 'quizzes', id));
 					setQuizzes(quizzes.filter((quiz) => quiz.id !== id));
 				},
 			},
@@ -29,7 +30,7 @@ export const Quiz = ({
 
 	return (
 		<View style={styles.container}>
-			<View style={{ width: "80%" }}>
+			<View style={{ width: '80%' }}>
 				<View style={{ paddingTop: 20, paddingLeft: 20 }}>
 					<Text style={styles.title}>{name}</Text>
 					<Text style={styles.description}>{description}</Text>
@@ -38,23 +39,24 @@ export const Quiz = ({
 				<Text style={styles.users}>Visible to {users.length} users</Text>
 			</View>
 			<View style={styles.rightsection}>
-				<View style={{ height: "100%" }}>
+				<View style={{ height: '100%' }}>
 					<TouchableOpacity
 						style={styles.editsection}
 						onPress={() =>
-							navigation.navigate("editquizpage", {
+							navigation.navigate('editquizpage', {
 								id: id,
 								title: name,
 								description: description,
+								duration: duration,
 								users: users,
 								questions: questions,
 							})
 						}
 					>
-						<FontAwesome name="edit" size={25} color={"#FFFFFF"} />
+						<FontAwesome name="edit" size={25} color={'#FFFFFF'} />
 					</TouchableOpacity>
 					<TouchableOpacity style={styles.deletesection} onPress={deleteQuiz}>
-						<FontAwesome name="trash" size={25} color={"#FFFFFF"} />
+						<FontAwesome name="trash" size={25} color={'#FFFFFF'} />
 					</TouchableOpacity>
 				</View>
 			</View>
--- src/screens/CreateQuizPage.js
+++ src/screens/CreateQuizPage.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 import {
 	ScrollView,
 	View,
@@ -6,32 +6,29 @@ import {
 	TextInput,
 	SafeAreaView,
 	TouchableOpacity,
-} from "react-native";
-import { styles } from "../styles/screens/CreateQuizStyle";
-import { MultipleSelectList } from "react-native-dropdown-select-list";
-import DateTimePicker from "@react-native-community/datetimepicker";
-import { db } from "../../firebaseConfig";
-import { collection, doc } from "firebase/firestore";
-import { GoBack, Question } from "../components/Index";
-import { fetchUsers } from "../utilities/fetchUsers";
-import { addQuiz } from "../utilities/addQuiz";
-import { addQuestions } from "../utilities/addQuestions";
-import FontAwesome from "@expo/vector-icons/FontAwesome";
+} from 'react-native';
+import { styles } from '../styles/screens/CreateQuizStyle';
+import { MultipleSelectList } from 'react-native-dropdown-select-list';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { db } from '../../firebaseConfig';
+import { collection, doc } from 'firebase/firestore';
+import { GoBack, Question } from '../components/Index';
+import { fetchUsers } from '../utilities/fetchUsers';
+import { addQuiz } from '../utilities/addQuiz';
+import { addQuestions } from '../utilities/addQuestions';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 const CreateQuizPage = ({ navigation, route }) => {
 	const [users, setUsers] = useState([]);
 	const [questions, setQuestions] = useState([]);
 	const [selected, setSelected] = useState([]);
-	const [title, setTitle] = useState("");
-	const [description, setDescription] = useState("");
-	const [duration, setDuration] = useState(0);
-	const [initial, setInitial] = useState({ hours: 0, seconds: 0 });
+	const [title, setTitle] = useState('');
+	const [description, setDescription] = useState('');
+	const [duration, setDuration] = useState({ hours: 0, minutes: 0 });
 
 	useEffect(() => {
 		setUsers([]);
 		fetchUsers(setUsers).catch((error) => console.log(error));
-		let temp = secondsToHoursAndMinutes(5400);
-		console.log(temp);
 	}, []);
 
 	useEffect(() => {
@@ -40,34 +37,20 @@ const CreateQuizPage = ({ navigation, route }) => {
 	}, [route.params]);
 
 	const saveQuiz = async () => {
-		if (title === "") return alert("Please enter title");
+		if (title === '') return alert('Please enter title');
 
-		const quizRef = doc(collection(db, "quizzes"));
+		const quizRef = doc(collection(db, 'quizzes'));
 		const userIds = selected.map(
 			(index) => users.find((user) => user.email === index).id
 		);
 		const questionIds = await addQuestions(quizRef.id, questions);
 
-		addQuiz(title, description, quizRef.id, userIds, questionIds);
-		navigation.navigate("managequizpage");
-	};
-
-	const setTime = (event, date) => {
-		let hoursToSeconds = date.getHours() * 3600;
-		let minutesToSeconds = date.getMinutes() * 60;
-		setDuration(hoursToSeconds + minutesToSeconds);
-		console.log(duration);
-	};
-
-	const secondsToHoursAndMinutes = (seconds) => {
-		let total = Math.floor(seconds / 60);
-		let minutes = Math.floor(total % 60);
-		let hours = Math.floor(total / 60);
-		return { hours, minutes };
+		addQuiz(title, description, duration, quizRef.id, userIds, questionIds);
+		navigation.navigate('managequizpage');
 	};
 
 	return (
-		<ScrollView bounces={false} style={{ backgroundColor: "#FFFFFF" }}>
+		<ScrollView bounces={false} style={{ backgroundColor: '#FFFFFF' }}>
 			<GoBack />
 			<Text style={styles.header}>Create Quiz</Text>
 			<SafeAreaView style={styles.container}>
@@ -76,24 +59,26 @@ const CreateQuizPage = ({ navigation, route }) => {
 					style={styles.input}
 					onChangeText={setTitle}
 					value={title}
-					placeholder={"Enter title"}
-					placeholderTextColor={"#757A86"}
+					placeholder={'Enter title'}
+					placeholderTextColor={'#757A86'}
 				/>
 				<Text style={styles.title}>Description</Text>
 				<TextInput
 					style={styles.input}
 					onChangeText={setDescription}
 					value={description}
-					placeholder={"Enter description"}
-					placeholderTextColor={"#757A86"}
+					placeholder={'Enter description'}
+					placeholderTextColor={'#757A86'}
 				/>
 				<Text style={styles.title}>Duration</Text>
 				<DateTimePicker
 					mode="time"
-					value={new Date(0, 0, 0, initial.hours, initial.minutes)}
+					value={new Date(0, 0, 0, duration.hours, duration.minutes, 0)}
 					display="default"
-					style={{ alignSelf: "flex-start", padding: 8 }}
-					onChange={setTime}
+					style={{ alignSelf: 'flex-start', padding: 8 }}
+					onChange={(e, d) =>
+						setDuration({ hours: d.getHours(), minutes: d.getMinutes() })
+					}
 				/>
 				<Text style={styles.title}>Visible to</Text>
 				<MultipleSelectList
@@ -108,7 +93,7 @@ const CreateQuizPage = ({ navigation, route }) => {
 					<Text style={styles.questions}>Questions</Text>
 					<TouchableOpacity
 						onPress={() =>
-							navigation.navigate("viewallquestionspage", {
+							navigation.navigate('viewallquestionspage', {
 								questions: questions,
 								setQuestions: setQuestions,
 							})
@@ -138,8 +123,8 @@ const CreateQuizPage = ({ navigation, route }) => {
 					<TouchableOpacity
 						style={styles.add}
 						onPress={() =>
-							navigation.navigate("addquestionpage", {
-								destination: "createquizpage",
+							navigation.navigate('addquestionpage', {
+								destination: 'createquizpage',
 							})
 						}
 					>
