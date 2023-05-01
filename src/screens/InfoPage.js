79eb009406a4496f961b8844145f28cc26bcf33e
anders-fm <92730849+anders-fm@users.noreply.github.com>
2023-05-01 15:56:32 +0200

    rm unused import

--- src/screens/InfoPage.js
+++ src/screens/InfoPage.js
@@ -5,7 +5,6 @@ import {
   TouchableOpacity,
   StatusBar,
   ImageBackground,
-  Button,
 } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { styles } from '../styles/screens/InfoStyle';
